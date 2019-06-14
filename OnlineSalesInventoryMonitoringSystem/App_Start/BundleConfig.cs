7dbef043f0c4e7a05cb86000a861f1bcc42f020e
John Oliver <oliverlacap1995@gmail.com>
2019-06-14 23:50:30 +0800

    Initialize UI

--- OnlineSalesInventoryMonitoringSystem/App_Start/BundleConfig.cs
+++ OnlineSalesInventoryMonitoringSystem/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace OnlineSalesInventoryMonitoringSystem
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery.min.js",
+                        "~/Scripts/jquery-ui.min.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,12 +22,38 @@ namespace OnlineSalesInventoryMonitoringSystem
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
+            //Other JS PlugIns
+            bundles.Add(new ScriptBundle("~/bundles/others").Include(
+                       "~/Scripts/raphael.min.js",
+                       "~/Scripts/morris.min.js",
+                       "~/Scripts/jquery.sparkline.min.js",
+                       "~/Scripts/jquery-jvectormap-1.2.2.min.js",
+                       "~/Scripts/jquery-jvectormap-world-mill-en.js",
+                       "~/Scripts/jquery.knob.min.js",
+                       "~/Scripts/moment.min.js",
+                       "~/Scripts/daterangepicker.js",
+                       "~/Scripts/bootstrap-datepicker.min.js",
+                       "~/Scripts/bootstrap3-wysihtml5.all.min.js",
+                       "~/Scripts/jquery.slimscroll.min.js",
+                       "~/Scripts/fastclick.js",
+                       "~/Scripts/adminlte.min.js"
+                       ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/ionicons.min.css",
+                      "~/Content/AdminLTE.min.css",
+                      "~/Content/morris.css",
+                      "~/Content/jquery-jvectormap.css",
+                      "~/Content/bootstrap-datepicker.min.css",
+                      "~/Content/daterangepicker.css",
+                      "~/Content/bootstrap3-wysihtml5.min.css"
+                      ));
         }
     }
 }
